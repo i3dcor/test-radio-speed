567da8dcfc4690f681a97d17ed0b6f4715a46d69
i3dcor <i3dcor@gmail.com>
2021-03-08 17:57:49 +0100

    send number codified left is integer part of number, right is decimals part

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
 input.onButtonPressed(Button.A, function () {
     radio.sendString("hola")
     basic.showString("ping")
@@ -6,6 +9,10 @@ radio.onReceivedString(function (receivedString) {
     if (receivedString == "hola") {
         basic.showString("pong")
     }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(10.21)
+    basic.showNumber((10.21 - Math.trunc(10.21)) * 100)
 })
 radio.setGroup(1)
 basic.forever(function () {
