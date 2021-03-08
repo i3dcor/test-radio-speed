02d89899d529cff6e668f937d61b85085798d7a8
i3dcor <i3dcor@gmail.com>
2021-03-08 11:35:57 +0100

    first test radio comm

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("hola")
+    basic.showString("ping")
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "hola") {
+        basic.showString("pong")
+    }
+})
+radio.setGroup(1)
 basic.forever(function () {
 	
 })
